7addc0fb1ee57c830d4cd3ab51cb73b8277787b3
Diana Rocha Botello <diana.botello.93@gmail.com>
2019-09-02 23:09:22 -0500

    Add Ejercicios 2.3 y 2.4 queda pendiente el corrector ortográfico

--- AnalisisFrecuencias.py
+++ AnalisisFrecuencias.py
@@ -6,13 +6,18 @@
 import nltk
 import collections
 from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
 class AnalisisFrecuencia():
 
 	def __init__(self):
 		self.tokenListbySentence = []
 		self.tokenList = []
+		self.tokenListStemming = []
+		self.tokenListLemmati = []
 		self.diccionarioDF = {}
+		self.lematizacionD = {}
 
 	def tokenizeFile (self, fileName):
 		toktok = ToktokTokenizer()
@@ -29,16 +34,88 @@ class AnalisisFrecuencia():
 				self.tokenListbySentence.append(tokenSentence)
 				for t in tokenSentence:
 					self.tokenList.append(t)
+			f.close()
 
-	def distribucionFrecuencias(self):
-		for token in self.tokenList:
+	def distribucionFrecuencias(self, tokenList):
+		self.diccionarioDF = {}
+		for token in tokenList:
 			if token not in self.diccionarioDF:
-				self.diccionarioDF.update({token:self.tokenList.count(token)})
-		self.diccionarioDF = sorted(self.diccionarioDF.items(), key=lambda x: len(x[0]))
+				self.diccionarioDF.update({token:tokenList.count(token)})
+		self.diccionarioDF = sorted(self.diccionarioDF.items(), key=lambda x: len(x[0]), reverse=True)
 		print(self.diccionarioDF)
+
+	def distribucionFrecuenciasInicial(self):
+		self.distribucionFrecuencias(self.tokenList)
+
+	def reduccionDimensionalidad(self):
+		print("Número de Tokens Antes de la Reducción: "+str(len(self.tokenList)))
+		for stopWord in stopwords.words("spanish"):
+			for token in self.tokenList:
+				if stopWord == token:
+					self.tokenList.remove(token)
+		print("Número de Tokens Después de la Reducción: "+str(len(self.tokenList)))
+
+	def distribucionFrecuenciasStemming(self):
+		stemmer = SnowballStemmer("spanish")
+		for t in self.tokenList:
+		# Obtener la raiz
+			self.tokenListStemming.append(stemmer.stem(t.lower()))
+		#print(self.tokenListStemming)
+		self.distribucionFrecuencias(self.tokenListStemming)
+
+	def getLematizacionDiccionario(self):
+		with open('lemmatization-es.txt', 'r', encoding='utf8') as f:
+			key = ''
+			values = []
+			for line in f:
+				l = line.split()
+				if key == '':
+					key = l[0]
+					values.append(l[1])
+				elif key == l[0]:
+					values.append(l[1])
+				else:
+					self.lematizacionD.update({key:values})
+					key = l[0]
+					values = []
+					values.append(l[1])
+			self.lematizacionD.update({key:values})
+			#print(self.lematizacionD)
+			f.close()
+
+	def distribucionFrecuenciasLematizacion(self):
+		self.getLematizacionDiccionario()
+		lematizado = False
+		for token in self.tokenList:
+			lematizado = False
+			for key, values in self.lematizacionD.items():
+				if token.lower() in values:
+					self.tokenListLemmati.append(key)
+					lematizado = True
+			if lematizado == False:
+				self.tokenListLemmati.append(token)
+		#print(self.tokenListLemmati)
+		self.distribucionFrecuencias(self.tokenListLemmati)
 
 if __name__ == "__main__":
 	a = AnalisisFrecuencia()
 	a.tokenizeFile("practica3.txt")
+	print("TOKENS DEL ARCHIVO practica3.txt")
 	print(a.tokenList)
-	a.distribucionFrecuencias()
+	print("-----------------------------------------------")
+	input("Press Enter to continue...")
+	print("Distribución de frecuencias practica3.txt")
+	a.distribucionFrecuenciasInicial()
+	print("-----------------------------------------------")
+	input("Press Enter to continue...")
+	print("Reducción de la Dimensionalidad practica3.txt")
+	a.reduccionDimensionalidad()
+	print("-----------------------------------------------")
+	input("Press Enter to continue...")
+	print("Distribución de frecuencias Stemming practica3.txt")
+	a.distribucionFrecuenciasStemming()
+	print("-----------------------------------------------")
+	input("Press Enter to continue...")
+	print("Distribución de frecuencias Lematización practica3.txt")
+	a.distribucionFrecuenciasLematizacion()
+	print("-----------------------------------------------")
